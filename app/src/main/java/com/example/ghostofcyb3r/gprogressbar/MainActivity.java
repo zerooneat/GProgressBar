6020cb619c3608124711b8d8ae41d333ffc0e7b0
testing <jonh@gmail.com>
2016-08-23 11:34:08 +0700

    Update MainActivity

--- app/src/main/java/com/example/ghostofcyb3r/gprogressbar/MainActivity.java
+++ app/src/main/java/com/example/ghostofcyb3r/gprogressbar/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.ghostofcyb3r.gprogressbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    Spinner spinner;
+    ArrayAdapter<CharSequence> arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner spinner = (Spinner)findViewById(R.id.spinner_country);
+        arrayAdapter = ArrayAdapter.createFromResource(this,R.array.country,android.R.layout.simple_spinner_item);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
+
     }
 }
